dbe53737f5bec16106b4308bd03fa633e3a2592e
Rizwan Trainings <rizwantrainingsb70p1@gmail.com>
2024-01-09 17:03:08 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/demo/b69demo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comdemo/b69demo/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
